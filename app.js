76c481caa1cd07e379207dfb4536429706e88141
Stanlee <stanz.ferigh@gmail.com>
2021-02-11 18:41:07 +1100

    displayed single meal page from api

--- app.js
+++ app.js
@@ -48,5 +48,63 @@ const searchMeal = (e) => {
   }
 }
 
+// Fetch meal by ID
+const getMealById = (mealID) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0]
+
+      addMealToDOM(meal)
+    })
+}
+
+const addMealToDOM = (meal) => {
+  const ingredients = []
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredients${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i} - ${meal[`strMeasure${i}`]}`]}`,
+      )
+    } else {
+      break
+    }
+  }
+
+  singleMeal.innerHTML = `
+    <div class='single-meal'>
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt=${meal.strMeal} />
+        <div class="single-meal-info">
+            ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+             ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+        </div>
+        <div class="main">
+            <p>${meal.strInstructions}</p>
+            <h2>Ingredients</h2>
+            <ul>
+                ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+            </ul>
+        </div>
+    </div>
+  `
+}
+
 // Event Listeners
 submit.addEventListener('submit', searchMeal)
+
+meals.addEventListener('click', (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info')
+    } else {
+      return false
+    }
+  })
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid')
+    getMealById(mealID)
+  }
+})
